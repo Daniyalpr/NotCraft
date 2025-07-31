e7a6b4402703ac97a3df58e8bb5f4022917fa87d
Daniyal <abdshahdaniyal@gmail.com>
2025-07-31 18:47:25 +0330

    feat: apply gravity and use config.FPS

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ logging.basicConfig(level = logging.INFO)
 pygame.init()
 
 clock = pygame.time.Clock()
-#Default settings
-FPS = 60
 # Display and title
 screen = pygame.display.set_mode((config.screen_width,config.screen_height))
 pygame.display.set_caption("NotCraft")
@@ -18,7 +16,7 @@ pygame.display.set_caption("NotCraft")
 background_surface = pygame.image.load("assets/graphics/enviorment/background.jpeg").convert()
 background_surface = pygame.transform.scale(background_surface,(config.screen_width,config.screen_height))
 
-player = player.Player((600,400),4,10)
+player = player.Player(startPos = (600,400), speed = 5, jump_speed = 3, gravity = 0.06)
 while True:
     #Groups
     player_group = pygame.sprite.GroupSingle(player)
@@ -31,5 +29,5 @@ while True:
     player_group.update()
 
     pygame.display.update()
-    clock.tick(FPS)
+    clock.tick(config.FPS)
 
