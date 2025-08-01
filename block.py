053e2db77b2ef72b7e11b7c33dce1e0f5177bf63
Daniyal <abdshahdaniyal@gmail.com>
2025-08-01 14:06:42 +0330

    refactor: move dirt block surface out of init function and make it static variable

--- block.py
+++ block.py
@@ -2,10 +2,10 @@ import pygame
 from config import block_size
 
 class Block(pygame.sprite.Sprite):
+    dirt_surface = pygame.transform.scale(pygame.image.load("assets/graphics/blocks/dirt.png"),(block_size,block_size))
     def __init__(self,default_pos:tuple):
         super().__init__()
-        self.image = pygame.image.load("assets/graphics/blocks/dirt.png")
-        self.image = pygame.transform.scale(self.image,(block_size,block_size))
+        self.image = Block.dirt_surface
         self.rect = self.image.get_rect(topleft = default_pos)
     def draw(self, screen):
         screen.blit(self.image,self.rect)
