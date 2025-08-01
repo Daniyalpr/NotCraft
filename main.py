076b5d511590c0f3472d556d1fbf33a214851cee
Daniyal <abdshahdaniyal@gmail.com>
2025-08-01 13:24:57 +0330

    feat: add debug mode

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import logging
 import config
 from sys import exit
-from colors import BLUE
+from colors import BLUE, WHITE
 import player
 from world import Terrain
 
@@ -23,16 +23,34 @@ player = player.Player(startPos = (600,400), speed = 5, jump_speed = 8, gravity 
 
 terrain1 = Terrain()
 terrain1.terrain_map[-1,-1] = 1
+
+
+DEFAULT_FONT = pygame.font.Font("assets/fonts/LuckiestGuy-Regular.ttf",30)
+debug_mode = True
 while True:
     #Groups
     player_group = pygame.sprite.GroupSingle(player)
 
-    print(int(player.rect.center[0]/60), int(player.rect.center[1]/60))
+
     #Draw things
     screen.blit(background_surface,(0,0))
     pygame.draw.rect(screen, 'Red', player.rect)
     player_group.draw(screen)
     terrain1.draw(screen)
+
+    if debug_mode:
+        txt_surface = DEFAULT_FONT.render(str(player.rect.center), True, WHITE)
+        screen.blit(txt_surface, (0,10))
+
+        txt_surface = DEFAULT_FONT.render(str(player.matrix_pos), True, WHITE)
+        screen.blit(txt_surface, (0,40))
+
+        txt_surface = DEFAULT_FONT.render("FPS: " + str(int(clock.get_fps())), True, WHITE)
+        screen.blit(txt_surface, (0,80))
+
+
+
+
 
     #if(player.rect.bottom >= dirtRec.top and player.rect.bottom - player.y_velocity <= dirtRec.top and player.rect.right > dirtRec.left and player.rect.left < dirtRec.right):
         #player.on_ground = True
