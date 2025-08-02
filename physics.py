e599dfe4dd911b08c499287cb4b74fdc30025ed0
Daniyal <abdshahdaniyal@gmail.com>
2025-08-02 14:02:32 +0330

    fix: player could go through blocks like ghost

--- physics.py
+++ physics.py
@@ -5,7 +5,7 @@ from block import Block
 import config
 
 class PhysicsBody():
-    def __init__(self, sprite:pygame.sprite.Sprite, gravity_value = 0.5, gravity_status = "ON", movement_speed = 1.5, jump_speed = 9, on_ground = False, y_velocity = 0, x_velocity = 0, x_force = 0.5, max_x_speed = 6, max_y_speed = 100):
+    def __init__(self, sprite:pygame.sprite.Sprite, gravity_value = 0.5, gravity_status = "ON", movement_speed = 0.3, jump_speed = 9, on_ground = False, y_velocity = 0, x_velocity = 0, x_force = 0.07, max_x_speed = 2.5, max_y_speed = 100):
         
         self.sprite = sprite
         self.gravity_value = gravity_value
@@ -35,26 +35,63 @@ class PhysicsBody():
                     self.sprite.rect.y -= self.max_y_speed
 
 
-    def apply_x_force(self):
+    def apply_movements(self, direction:str):
+        if direction == "RIGHT":
+            block1 = [int(self.sprite.rect.top/120), self.sprite.center_matrix_pos + 1]
+            right_blocks_position = []
+
+    def apply_x_force(self, ):
         self.sprite.rect.x += self.x_velocity
 
         if self.x_velocity > 0:
             self.x_velocity -= self.x_force
         elif self.x_velocity < 0:
             self.x_velocity += self.x_force
-        if abs(self.x_velocity) < 1:
+
+
+        #if player speed is less than some speed (in this exapmle 0.2) player will stop
+        if abs(self.x_velocity) < 0.2:
             self.x_velocity = 0
 
     #The next function checks if player is on ground or not
     def update_player_state(self, terrain_map):
-        below_block_idx = self.sprite.matrix_pos + np.array([1,0])
+        below_block_idx = self.near_blocks_matrix_pos()[-1,1]
         
         if below_block_idx[0] <= config.block_num_row -1 and terrain_map[below_block_idx[0],below_block_idx[1]] != 0 and self.sprite.rect.bottom >= Block.top(below_block_idx) and self.sprite.rect.bottom-self.y_velocity <= Block.top(below_block_idx):
             self.on_ground = True
+            self.sprite.rect.bottom = Block.top(below_block_idx)
         elif below_block_idx[0] <= config.block_num_row -1:
             self.on_ground = False
+
+
+    def update_position(self, terrain_map:np.ndarray):
+        self.sprite.rect.x += self.x_velocity
+        ### we will add to, position and after that we will check if we got through front blocks and if yes we get back
+        right_blocks_position = self.near_blocks_matrix_pos()[1:3,1:3]
+        for rows in right_blocks_position:
+            for block_pos in rows:
+                if terrain_map[block_pos[0],block_pos[1]] != 0:
+                    if self.sprite.rect.right >= Block.left(block_pos) and self.sprite.rect.right - self.x_velocity <= Block.left(block_pos):
+                        self.x_velocity = 0
+                        self.sprite.rect.right = Block.left(block_pos)
+        ####
+
+        left_blocks_position = self.near_blocks_matrix_pos()[1:3,0:2]
+
+        for rows in left_blocks_position:
+            for block_pos in rows:
+                if terrain_map[block_pos[0],block_pos[1]] != 0:
+                    if self.sprite.rect.left <= Block.right(block_pos) and self.sprite.rect.right - self.x_velocity >= Block.right(block_pos):
+                        self.x_velocity = 0
+                        self.sprite.rect.left= Block.right(block_pos)
+
+ 
+
+
+
+
     
-    def move(self, direction:str):
+    def move(self, direction:str, terrain_map:np.ndarray):
         if direction == "UP":
             self.sprite.rect.y -= self.movement_speed
             self.on_ground = False
@@ -67,6 +104,7 @@ class PhysicsBody():
                 self.x_velocity += self.movement_speed
             else:
                 self.x_velocity = self.max_x_speed
+
         elif direction == "LEFT":
             if abs(self.x_velocity - self.movement_speed) < self.max_x_speed:
                 self.x_velocity -= self.movement_speed
@@ -75,6 +113,19 @@ class PhysicsBody():
         else:
             logging.error("Direction must be UP, DOWN, LEFT, RIGHT")
     
+
+    # returns 2d numpy array that contains position of 9 
+    def near_blocks_matrix_pos(self):
+        positions = np.empty((4,3), tuple)
+        #the below variable is for adding to rows and columns each time in for loop
+        default_tuple = np.array([0,0])
+        first_pos = np.array([int(self.sprite.rect.top/60) - 1, self.sprite.center_matrix_pos[1] - 1])
+        for idx in np.ndindex(positions.shape):
+            positions[idx[0],idx[1]] = np.array(idx) + first_pos
+        return positions
+
+
+
 
                         
 
