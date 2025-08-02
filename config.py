2fd6385c7157299812d1d7cbaeb45c8ed3f3d9f2
Daniyal <abdshahdaniyal@gmail.com>
2025-08-02 13:56:53 +0330

    style: change character size for better collisions

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ block_num_column = int(screen_width / block_size)
 
 FPS = 60
 
-character_width = block_size - 5
-character_height = block_size * 2 - 5
+character_width = block_size
+character_height = block_size * 2
 
