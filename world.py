8dee05871541506bbed6a34c757188a0fd798bdd
Daniyal <abdshahdaniyal@gmail.com>
2025-08-03 23:07:27 +0330

    refactor: different terrain

--- world.py
+++ world.py
@@ -7,22 +7,18 @@ class Terrain():
     def __init__(self):
         self.terrain_map = self.terrain_generator()
     def terrain_generator(self) -> np.ndarray:
-        map = np.zeros((block_num_row,block_num_column),int)
-        map[-1] = np.ones(block_num_column,int)
-        map[-2,-5:-1] = 1
-        map[-3,-4] = 1
-        map[-4,-3] = 1
-        random_row = np.random.randint(0, 18, 10)
-        random_column= np.random.randint(0, 32, 10)
-        map[random_row,random_column] = 1
-        #generate some random blocks
+        map = np.zeros((block_num_row,block_num_column), dtype = object)
+        for col in range(block_num_column):
+            map[block_num_row -1, col] = Block((block_num_row -1, col), "dirt")
+        for col in range(block_num_column):
+            map[0, col] = Block((0, col), "dirt")
+        map[-2, -5] = Block((block_num_row -2, block_num_column -5), "dirt")
+        map[-3, -4] = Block((block_num_row -3, block_num_column -4), "dirt")
         return map
     def draw(self, screen):
-        ## The next for loop goes through all indexes in numpy 2d array
         for idx in np.ndindex(self.terrain_map.shape):
             if self.terrain_map[idx] != 0:
-            #When you multiply row idx with block_size and column idx with block size,blocks fit perfectly together
-                Block((idx[1]*block_size, idx[0]*block_size)).draw(screen)
+                self.terrain_map[idx].draw(screen)
             
 
                     
