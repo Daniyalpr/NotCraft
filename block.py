2fa2d507291053a34a695e1f156711264cc455b6
Daniyal <abdshahdaniyal@gmail.com>
2025-08-04 22:59:50 +0330

    feat: make pos optional

--- block.py
+++ block.py
@@ -4,13 +4,15 @@ from config import block_size
 class Block(pygame.sprite.Sprite):
     block_surface = {"dirt":pygame.transform.scale(pygame.image.load("assets/graphics/blocks/dirt.png"),(block_size,block_size))
 }
-    def __init__(self,start_matrix_pos:tuple, block_name:str):
+    def __init__(self, block_name:str, start_matrix_pos:tuple = (0, 0)):
         super().__init__()
         self.image = Block.block_surface[block_name]
         self.matrix_pos = start_matrix_pos
         self.rect = self.image.get_rect(topleft = (self.matrix_pos[1]*block_size, self.matrix_pos[0]*block_size))
     def draw(self, screen):
-        screen.blit(self.image,self.rect)
+
+        self.rect = self.image.get_rect(topleft = (self.matrix_pos[1]*block_size, self.matrix_pos[0]*block_size))
+        screen.blit(self.image, self.rect)
 
     
 
